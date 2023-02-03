b9801c56f5dbc1b59fd64883c165d5e4d17eff99
keishaharris <keishadharriss@gmail.com>
2023-02-02 22:25:54 -0500

    refractor about html to JS component

---
+++ src/about.js
@@ -0,0 +1,17 @@
+function about () {
+const element = document.createElement('div');
+element.classList.add('about-ctn');
+const h1 = document.createElement('h1');
+h1.innerText = 'about farm market.';
+
+const span =document.createElement('span');
+span.classList.add('about-txt');
+span.innerText= `we love all things health. farm market was created for you health needs. 
+we care about providing you with the most nutritious and healthies smoothies and
+shakes in the city! Come stop by and support our health movement. `
+
+element.appendChild(h1, span)
+
+}
+
+export default about;
