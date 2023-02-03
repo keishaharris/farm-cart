efa238f84b8ae42df5414e89665f188d529dae72
keishaharris <keishadharriss@gmail.com>
2023-02-03 17:01:10 -0500

    refractor footer html element to footer module

---
+++ src/footer.js
@@ -0,0 +1,9 @@
+function footer(){
+    const footer = document.createElement('FOOTER');
+    footer.classList.add('footer-size');
+    footer.innerText = 'farm cart 2023 \u00A9 | Created by Keisha Harris';
+return footer
+
+}
+
+export default footer;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import header from './header.js';
+import footer from './footer.js';
 
 const container = document.getElementById('container');
 container.append(header());
+
+const mainCtn = document.createElement('MAIN');
+mainCtn.classList.add('main-size');
+
+const mainDiv = document.createElement('div')
+mainDiv.classList.add('main-ctn');
+
+mainCtn.append(mainDiv);
+container.append(mainCtn);
+
+container.append(footer());
 
